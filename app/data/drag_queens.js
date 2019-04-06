c9bf473c776f7f2ac64551b77aae54241ceaf7f2
personal computer <bret.allan@gmail.com>
2019-04-05 18:05:50 -0700

    added image source

--- app/data/drag_queens.js
+++ app/data/drag_queens.js
@@ -3,13 +3,13 @@ let dragQueens = [
   {
     name: "Trixie Mattel",
     job: "Drag Queen",
-    photo: "",
+    photo: "https://github.com/bretgithub/DragD/blob/master/trixie.jpg",
     scores: ["1", "5", "5", "1", "1", "1"]
   },
   {
     name: "Katya Zamolodchikova",
     job: "Drag Queen",
-    photo: "",
+    photo: "https://github.com/bretgithub/DragD/blob/master/katya.jpg",
     scores: ["5", "1", "2", "5", "4", "5"]
   }
 ];
